b35754ad0b953f7e3bcd4c16248845666b50165c
Johnny Soong <johnnysoong@hotmail.com>
2018-05-22 04:38:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,19 +20,7 @@ function sayHiToGrandma(string) {
   } else if (string.toUpperCase() === string) {
     return "YES INDEED!";
   } else if (string === "I love you, Grandma.") {
-    
+    return "I love you, too.";
   }
 }
 
-function sayHiToGrandma(string) {
-  if (string.toUpperCase() === string)  {
-    
-  }
-}
-
-
-function sayHiToGrandma (string) {
-  if (string === "I love you, Grandma.") {
-    
-  }
-}
