626fec40e294f6b8739eec58ed709d5fe2574432
Johnny Soong <johnnysoong@hotmail.com>
2018-05-22 04:21:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    return 'I can\'t hear you!';
+    console.log('I can\'t hear you!');
   }
 }
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
-    return "YES INDEED!";
+    console.log("YES INDEED!");
   }
 }
 
